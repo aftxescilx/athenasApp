fd689b826864c6120248255332222479259ff75e
aftxescilx <miriaam.priscilaa@gmail.com>
2023-12-12 00:16:38 -0600

    library

--- athenas/athenas/Views/HomePage.xaml.cs
+++ athenas/athenas/Views/HomePage.xaml.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace athenas.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
-        public HomePage ()
+        public HomePage()
         {
             InitializeComponent();
         }
